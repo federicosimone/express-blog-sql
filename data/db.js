38a85b6f51f03116012b46120b6ecf9ad793b7d5
federicosimone <federico.simone15@gmail.com>
2026-03-12 23:20:06 +0100

    creation db file

---
+++ data/db.js
@@ -0,0 +1,19 @@
+const mysql = require("mysql2");
+
+const dbConfiguration = {
+    host: "localhost",
+    user: "root",
+    password: "root",
+    database: "blog"
+}
+
+function onDatabaseConnection(error) {
+    if (error) throw error;
+
+    console.log("Connessione a MySQL avvenuta con successo!");
+}
+
+const db = mysql.createConnection(dbConfiguration);
+db.connect(onDatabaseConnection);
+
+module.export = dbConnection
