e6d86516d2872e4136eb1ab27d318e85f251c82f
federicosimone <federico.simone15@gmail.com>
2026-03-17 21:04:59 +0100

    piccole modifiche

--- data/db.js
+++ data/db.js
@@ -4,11 +4,11 @@ const dbConfiguration = {
     host: "localhost",  //di default gira sulla porta localhost3306
     user: "root",
     password: "root",
-    database: "blog" //nome delo schema 
+    database: "blog" //nome dello schema 
 }
 
 function onDatabaseConnection(error) {
-    if (error) throw error;
+    if (error) throw error;  //se non c'è errore vedrò la stringa stampata sotto. 
 
     console.log("Connessione a MySQL avvenuta con successo!");  //questa frase viene stampata una volta che lancio express, npm run dev
 }
